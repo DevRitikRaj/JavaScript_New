65e0d3283a780f3d6da211924e4543898e4d17f3
DevRitikRaj <rj1131999@gmail.com>
2024-01-17 00:57:39 +0530

    DataTypes

---
+++ DataTypes.js
@@ -0,0 +1,12 @@
+let name="Ritik" 
+ //String
+// BigInt
+// undefined
+// null
+// Boolean
+// Number
+// symbol
+
+console.log(typeof(name))
+console.log(typeof(null));
+console.log(typeof(null));
