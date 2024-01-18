b1d46c9b885d6c20cbf0f196a117803342304481
DevRitikRaj <rj1131999@gmail.com>
2024-01-18 18:47:09 +0530

    Day 2

---
+++ TypesOfDatatypes.js
@@ -0,0 +1,30 @@
+// Two types fo Datatypes
+
+
+// 1 Primitive
+// 2. Non-Primitive
+
+
+// Primitive -> Number, String, Boolean, BigInt, null, undefined, Symbol
+
+
+// Non-Primitive -> Array, Object and Function
+
+// const big=2222222222222222n;
+// console.log(big);
+// console.log(typeof big);
+
+let arr=["ritik", 12,"12"];
+
+const oj={
+    name:"RItik",
+    roll:2808
+}
+
+const myfun=function(){
+    console.log("My name is Ritik");
+}
+console.log(arr)
+console.log(oj)
+myfun();
+
