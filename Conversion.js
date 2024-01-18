cd58e855336a3d80fcb0f9de2289e1eb4178e778
DevRitikRaj <rj1131999@gmail.com>
2024-01-18 17:42:46 +0530

    Conversion Day 2

---
+++ Conversion.js
@@ -0,0 +1,30 @@
+// Num => String
+// let numb=33
+// let num= String (22);
+// console.log(numb);
+// console.log(typeof num);
+
+// String => Number
+
+// let str="Ritik"
+// console.log( typeof str);
+// let strnum= Number("Ritik")
+// console.log( typeof strnum); 
+
+// let a="12n";
+// let conv= Number (a)
+// console.log( conv); // NaN
+// console.log(typeof conv)
+
+// when assign to NULL to give 0
+
+//boolean
+
+let bol=true;
+let convr= String (bol)
+console.log( typeof convr);
+console.log(bol);
+
+
+
+
