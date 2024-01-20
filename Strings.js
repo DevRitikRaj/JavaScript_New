010a5ad6214f90c9162994281859cda730350cbb
DevRitikRaj <rj1131999@gmail.com>
2024-01-20 16:47:24 +0530

    Javascript day 3

--- Strings.js
+++ Strings.js
@@ -25,6 +25,42 @@ console.log(replaced.split('.'))
 // substring()               Yes                               Treated as 0
 // slice()                     No                               Counted from the end of the string
 
+// Lecture 12
+
+const num=100.24;
+console.log(num.toFixed(0))
+console.log(num.toPrecision(3))
+const number=1000000;
+console.log(number.toLocaleString('en-IN'));
+
+// -------------------------Maths-------------------------
+
+// console.log(Math)
+// console.log(Math.abs(-2));
+// console.log(Math.round(3.5))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.max(1,2,3));
+// console.log(Math.min(1,2,3))
+
+
+// Random Values
+
+console.log(Math.random())
+console.log(Math.random()*10 +1)
+console.log(Math.floor(Math.random()*10 +1))
+
+const min =10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+
+
+
+
+
+
+
 
 
 
