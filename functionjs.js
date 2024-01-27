72770d9987570918b5985b7a9e3e36666730545c
DevRitikRaj <rj1131999@gmail.com>
2024-01-27 21:05:15 +0530

    Function advanced Concept

---
+++ functionjs.js
@@ -0,0 +1,54 @@
+// function myName(){
+//     console.log("R");
+//     console.log("I");
+//     console.log("T");
+//     console.log("I");
+//     console.log("K");
+    
+// }
+// myName()
+// myName()
+
+// function addTwo(one, two){
+//     if(one===two){
+//         console.log(one+two);
+
+//     }
+//     else{
+//         console.log("NaNN");
+//     }
+
+
+   
+// }
+
+// addTwo(10,10)
+
+
+
+
+// function add(one,two){
+//     console.log(one + two);
+// }
+// const store=add(11,21)
+// console.log("Result is: ",store);
+
+function add(one,two){
+    let Result=one+two;
+    return Result;
+}
+const store=add(11,21)
+console.log("Result is: ",store);
+
+function login(username){
+
+    if(!username){
+        console.log("please login");
+        return;
+    }
+
+
+
+    return `${username} just logged In`
+}
+console.log(login(""));
