9b8618b23cf5f7b9021c376aeab62d0971a69491
DevRitikRaj <rj1131999@gmail.com>
2024-01-29 18:07:07 +0530

    CallStack.js

---
+++ CallStack.js
@@ -0,0 +1,13 @@
+let val1=12
+let val2=21
+
+function addTwo(num1, num2){
+    let total=num1+num2;
+
+    return num1+num2;
+
+}
+
+
+console.log(result1=addTwo(val1,val2)); 
+console.log(result2=addTwo(10,2)); 
